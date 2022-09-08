480bca4c01a13002dbbbfc9567a0be3a618c118e
byKriz <cristianesquivelh@hotmail.com>
2022-09-08 17:26:30 -0400

    importando el todoForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import { AiOutlinePlus } from "react-icons/ai";
 import { TodoItem } from "./components/TodoItem";
+import { TodoForm } from "./components/TodoForm";
 
 const style = {};
 
 function App() {
-  const [todos, setTodos] = useState(["Lear React", "Learn Firebase"]);
+  const [todosList, setTodosList] = useState(["Lear React", "Learn Firebase"]);
 
   return (
     <div className="h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]">
@@ -13,18 +13,13 @@ function App() {
         <h1 className="text-4xl font-bold text-center text-gray-800 p-2">
           Todo App
         </h1>
-        <form className="flex justify-between mx-4">
-          <input className='border p-2 w-full text-xl' type="text" />
-          <button className='border p-4 ml-2 bg-purple-500 text-slate-100'>
-            <AiOutlinePlus size={30} />
-          </button>
-        </form>
+        <TodoForm />
         <ul>
-          {todos.map((todo) => (
+          {todosList.map((todo) => (
             <TodoItem key={todo} text={todo} />
           ))}
         </ul>
-        <p className="text-center p-2">You have {todos.length} todos</p>
+        <p className="text-center p-2">You have {todosList.length} todos</p>
       </div>
     </div>
   );
